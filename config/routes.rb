e0ca537fc69825e0687dfe8da627110080acfedd
David Healy <drtjprdave@gmail.com>
2022-12-08 13:42:46 -0700

    Init formatting of edit user and sign up pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+# This setes up redirection after signing out.
+devise_scope :user do
+  get '/users', to: 'devise/registration#new'
+  get '/users/password', to: 'devise/password#new'
+  get '/users/sign_out' => 'devise/sessions#destroy'
+end
+  devise_for :users
+
   get 'home/about'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
